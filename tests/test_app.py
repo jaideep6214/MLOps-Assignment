43ff74e7cd2ba34039d782ab9381b352144609f8
jaideep6214 <43785047+jaideep6214@users.noreply.github.com>
2025-01-27 22:56:02 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,9 +5,7 @@ import sys
 import os
 from app import app
 
-# Add the src directory to the Python path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                             "../src")))
+
 
 
 @pytest.fixture
